b594ca88540564b17d1d285ebb5d0a1baab84e11
Mill9510 <mill9510@stud.kea.dk>
2023-09-20 18:51:25 +0200

    animation

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ document.addEventListener("mousemove", (e) => {
   cursor.style.left = `${e.clientX + window.scrollX}px`;
   cursor.style.top = `${e.clientY + window.scrollY}px`;
 });
+
+/**Linjen henter alle elementerne på siden, som har klassen "slide-in" og gemmer dem som const "sliders". **/
+const sliders = document.querySelectorAll(".slide-in");
+/**Bestemmer hvordan og hvor meget elementerne vises på skærmen */
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px",
+};
+
+/**Udløser appear ved scroll** */
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
