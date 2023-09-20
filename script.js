c94dda1830a5b4d71d67ebd2c1972316960cea44
Mill9510 <mill9510@stud.kea.dk>
2023-09-20 19:52:36 +0200

    footer, links

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ document.addEventListener("mousemove", (e) => {
   cursor.style.top = `${e.clientY + window.scrollY}px`;
 });
 
-/**Linjen henter alle elementerne på siden, som har klassen "slide-in" og gemmer dem som const "sliders". **/
+//Linjen henter alle elementerne på siden, som har klassen "slide-in" og gemmer dem som const "sliders".
 const sliders = document.querySelectorAll(".slide-in");
-/**Bestemmer hvordan og hvor meget elementerne vises på skærmen */
+//Bestemmer hvordan og hvor meget elementerne vises på skærmen
 const appearOptions = {
   threshold: 0,
   rootMargin: "0px 0px -100px 0px",
 };
 
-/**Udløser appear ved scroll** */
+//Udløser appear ved scroll
 const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
