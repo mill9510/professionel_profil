d98471f492ffc7247fa7036e99475c02c64ed0a2
Mill9510 <mill9510@stud.kea.dk>
2023-09-23 09:59:42 +0200

    styles, indhold

--- script.js
+++ script.js
@@ -27,3 +27,18 @@ const appearOnScroll = new IntersectionObserver(function (entries, appearOnScrol
 sliders.forEach((slider) => {
   appearOnScroll.observe(slider);
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
